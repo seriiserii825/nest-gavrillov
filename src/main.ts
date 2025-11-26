67f9debf0b24b25a0457df4126c3084e2485855e
serii <seriiburduja@gmail.com>
2025-11-26 17:25:11 +0200

    feat: first steps

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
