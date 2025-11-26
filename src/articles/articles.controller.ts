df26b823acca3f04f102d0efe288764a9513f875
serii <seriiburduja@gmail.com>
2025-11-26 17:50:12 +0200

    feat: articles controller and servie

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {ArticlesService} from './articles.service';
 
 @Controller('articles')
 export class ArticlesController {
+  constructor(private service: ArticlesService) {}
+  
   @Post()
   create() {
-    return 'This action adds a new article';
+    return this.service.create();
+  }
+
+  @Get()
+  getList() {
+    return this.service.getList();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: number) {
+    return this.service.getById(id);
+  }
+
+  @Put(':id')
+  updateById(@Param('id') id: number) {
+    return this.service.updateById(id);
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: number) {
+    return this.service.deleteById(id);
   }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -2,5 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ArticlesService {
-
+  create() {
+    return 'This action adds a new article';
+  }
+  getList() {
+    return 'This action returns all articles';
+  }
+  getById(id: number) {
+    return `This action returns article #${id}`;
+  }
+  updateById(id: number) {
+    return `This action updates article #${id}`;
+  }
+  deleteById(id: number) {
+    return `This action deletes article #${id}`;
+  }
 }
